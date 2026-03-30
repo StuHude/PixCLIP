6a4e12ea83ee89ee0a574ce29bdb2022b5ed9ae9
chenyu <923175027@qq.com>
2026-03-30 16:39:46 +0800

    polish

--- eval/Urban1k/load_urbank.py
+++ eval/Urban1k/load_urbank.py
@@ -1,32 +1,40 @@
 import os
 import numpy as np
 from PIL import Image
-from torch.utils.data import Dataset,DataLoader
+from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
 PIXEL_MEAN = [0.48145466, 0.4578275, 0.40821073]
 PIXEL_STD = [0.26862954, 0.26130258, 0.27577711]
-xyc_test=[0.48145466]
-xyc_test1=[0.26862954]
+
+
 class UrbankDataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.img_dir = os.path.join(root_dir, "image")
         self.caption_dir = os.path.join(root_dir, "caption")
-        
-        self.transform = transform or transforms.Compose([
-            transforms.Resize((224, 224), interpolation=Image.BICUBIC),
-            transforms.ToTensor(),
-            transforms.Normalize(PIXEL_MEAN, PIXEL_STD),
-        ])
 
-        self.mask_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize((224, 224)),
-            #transforms.Normalize(0.5, 0.26),
-        ])
+        self.transform = transform or transforms.Compose(
+            [
+                transforms.Resize((224, 224), interpolation=Image.BICUBIC),
+                transforms.ToTensor(),
+                transforms.Normalize(PIXEL_MEAN, PIXEL_STD),
+            ]
+        )
+
+        self.mask_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize((224, 224)),
+                # transforms.Normalize(0.5, 0.26),
+            ]
+        )
 
         # 获取所有图像文件名（不含扩展名）
-        self.image_ids = [os.path.splitext(f)[0] for f in os.listdir(self.img_dir) if f.endswith('.jpg')]
+        self.image_ids = [
+            os.path.splitext(f)[0]
+            for f in os.listdir(self.img_dir)
+            if f.endswith(".jpg")
+        ]
         self.image_ids.sort()  # 按数字顺序排序
 
     def __len__(self):
@@ -52,13 +60,12 @@ class UrbankDataset(Dataset):
         return img_tensor, mask_tensor, caption_value
 
 
-if __name__=="__main__":
-    root_dir="data/Urban1k"
-    dataset=UrbankDataset(root_dir)
-    dataloader=DataLoader(dataset, batch_size=4, shuffle=True)
-    for img,mask,caption in dataloader:
-        print(f'image size: {img.shape}')
-        print(f'mask size: {mask.shape}')
-        print(f'caption: {caption}')
+if __name__ == "__main__":
+    root_dir = "data/Urban1k"
+    dataset = UrbankDataset(root_dir)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+    for img, mask, caption in dataloader:
+        print(f"image size: {img.shape}")
+        print(f"mask size: {mask.shape}")
+        print(f"caption: {caption}")
         break
-    
